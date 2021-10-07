2ee0b277374c27858a0ba5320b3145b374aa94f9
fillipe.felix <felipesoares_1993@hotmail.com>
2021-10-07 14:00:14 -0300

    creation of jwt token issuance

--- src/services/EE.Identidade.API/Controllers/AuthController.cs
+++ src/services/EE.Identidade.API/Controllers/AuthController.cs
@@ -1,7 +1,17 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using EE.Identidade.API.Extensions;
 using EE.Identidade.API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
 namespace EE.Identidade.API.Controllers
 {
@@ -11,14 +21,23 @@ namespace EE.Identidade.API.Controllers
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly AppSettings _appSettings;
 
-        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, IOptions<AppSettings> appSettings)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _appSettings = appSettings.Value;
         }
 
+        /// <summary>
+        /// Realizar o cadastro de um usuario
+        /// </summary>
+        /// <param name="usuarioRegistro"></param>
+        /// <returns></returns>
         [HttpPost("nova-conta")]
+        [ProducesResponseType(typeof(OkResult), StatusCodes.Status200OK)]
+        [ProducesResponseType( StatusCodes.Status400BadRequest)]
         public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
         {
             if (!ModelState.IsValid)
@@ -38,13 +57,19 @@ namespace EE.Identidade.API.Controllers
             if (result.Succeeded)
             {
                 await _signInManager.SignInAsync(user, false);
-                return Ok();
+                return Ok(await GerarJwt(usuarioRegistro.Email));
             }
 
             return BadRequest();
         }
-        
+        /// <summary>
+        /// Realizar o login de um usuario
+        /// </summary>
+        /// <param name="usuarioLogin"></param>
+        /// <returns></returns>
         [HttpPost("autenticar")]
+        [ProducesResponseType(typeof(OkResult), StatusCodes.Status200OK)]
+        [ProducesResponseType( StatusCodes.Status400BadRequest)]
         public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
         {
             if (!ModelState.IsValid)
@@ -56,10 +81,79 @@ namespace EE.Identidade.API.Controllers
 
             if (result.Succeeded)
             {
-                return Ok();
+                return Ok(await GerarJwt(usuarioLogin.Email));
             }
 
             return BadRequest();
+        }
+
+        /// <summary>
+        /// Gera um token JWT para o usuario logado
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        private async Task<UsuarioRespostaLogin> GerarJwt(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            var claims = await _userManager.GetClaimsAsync(user);
+            var userRoles = await _userManager.GetRolesAsync(user);
+            
+            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Jti, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+
+            foreach (var userRole in userRoles)
+            {
+                claims.Add(new Claim("role", userRole));
+            }
+
+            var identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            //var ser o manipulador que vai gerar minha chave
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _appSettings.Emissor,
+                Audience = _appSettings.ValidoEm,
+                Subject = identityClaims,
+                Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            var encodedToken = tokenHandler.WriteToken(token);
+
+            var response = new UsuarioRespostaLogin
+            {
+                AccessToken = encodedToken,
+                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+                UsuarioToken = new UsuarioToken
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c => new UsuarioClaim
+                    {
+                        Type = c.Type,
+                        Value = c.Value
+                    })
+                }
+            };
+
+            return response;
+
+        }
+
+        private static long ToUnixEpochDate(DateTime dateTime)
+        {
+            var result = (long)Math.Round((dateTime.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero))
+                .TotalSeconds);
+
+            return result;
         }
     }
 }
--- src/services/EE.Identidade.API/Models/UserViewModels.cs
+++ src/services/EE.Identidade.API/Models/UserViewModels.cs
@@ -1,7 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EE.Identidade.API.Models
 {
+    /// <summary>
+    /// Classe para cadastro de um usuario
+    /// </summary>
     public class UsuarioRegistro
     {
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
@@ -16,6 +20,9 @@ namespace EE.Identidade.API.Models
         public string SenhaConfirmacao { get; set; }
     }
 
+    /// <summary>
+    /// Classe para login de usuario
+    /// </summary>
     public class UsuarioLogin
     {
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
@@ -25,5 +32,34 @@ namespace EE.Identidade.API.Models
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
         public string Senha { get; set; }
+    }
+
+    /// <summary>
+    /// Class que tem a resposta do login que retorna o JWT e dados de quem esta logado
+    /// </summary>
+    public class UsuarioRespostaLogin
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public UsuarioToken UsuarioToken { get; set; }
+    }
+
+    /// <summary>
+    /// Class que retorna o usuario que tem informações do JWT
+    /// </summary>
+    public class UsuarioToken
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<UsuarioClaim> Claims { get; set; }
+    }
+
+    /// <summary>
+    /// Class que tem os dados das claims desse usuario
+    /// </summary>
+    public class UsuarioClaim
+    {
+        public string Value { get; set; }
+        public string Type { get; set; }
     }
 }
