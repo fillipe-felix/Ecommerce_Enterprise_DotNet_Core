12d06cb5ebe32822fc76ebe0dbffe37d1973c0da
fillipe.felix <felipesoares_1993@hotmail.com>
2021-10-07 15:15:23 -0300

    add comments in MainController.cs

--- src/services/EE.Identidade.API/Controllers/MainController.cs
+++ src/services/EE.Identidade.API/Controllers/MainController.cs
@@ -10,6 +10,11 @@ namespace EE.Identidade.API.Controllers
     {
         protected ICollection<string> Errors = new List<string>();
 
+        /// <summary>
+        /// Response customizada para retorno de controllers personalizados
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns></returns>
         protected ActionResult CustomResponse(object result = null)
         {
             if (OperacaoValida())
@@ -23,6 +28,11 @@ namespace EE.Identidade.API.Controllers
             }));
         }
 
+        /// <summary>
+        /// Response customizada para retorno de controllers personalizados para ModelStates
+        /// </summary>
+        /// <param name="modelStateDictionary"></param>
+        /// <returns></returns>
         protected ActionResult CustomResponse(ModelStateDictionary modelStateDictionary)
         {
             var erros = modelStateDictionary.Values.SelectMany(e => e.Errors);
@@ -34,16 +44,27 @@ namespace EE.Identidade.API.Controllers
             return CustomResponse();
         } 
 
+        /// <summary>
+        /// Veifica se existe erros
+        /// </summary>
+        /// <returns></returns>
         protected bool OperacaoValida()
         {
             return !Errors.Any();
         }
 
+        /// <summary>
+        /// Adiciona erros dentro da collection Errors
+        /// </summary>
+        /// <param name="erro"></param>
         protected void AdicionarErroProcessamento(string erro)
         {
             Errors.Add(erro);
         }
 
+        /// <summary>
+        /// Limpa erros dentro da conllection Errors
+        /// </summary>
         protected void LimparErrosProcessamento()
         {
             Errors.Clear();
