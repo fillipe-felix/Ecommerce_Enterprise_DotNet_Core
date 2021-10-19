e97866a80a809549c4715d7866f32bb346403afe
fillipe.felix <felipesoares_1993@hotmail.com>
2021-10-19 20:28:20 -0300

    adding a DelegatingHandler to intercept requests and add an Authorization header

--- src/web/EE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/EE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,5 +1,6 @@
 ﻿using EE.WebApp.MVC.Extensions;
 using EE.WebApp.MVC.Services;
+using EE.WebApp.MVC.Services.Handlers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,8 +10,12 @@ namespace EE.WebApp.MVC.Configuration
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            //Utiliza transient pq é chamado uma estancia de cada vez
+            services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+            
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
-            services.AddHttpClient<ICatalogoService, CatalogoService>();
+            services.AddHttpClient<ICatalogoService, CatalogoService>()
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
---
+++ src/web/EE.WebApp.MVC/Services/Handlers/HttpClientAuthorizationDelegatingHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using EE.WebApp.MVC.Extensions;
+
+namespace EE.WebApp.MVC.Services.Handlers
+{
+    public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
+    {
+        private readonly IUser _user;
+
+        public HttpClientAuthorizationDelegatingHandler(IUser user)
+        {
+            _user = user;
+        }
+
+        /// <summary>
+        /// Faz override do metodo SendAsync e em todas as requisições que forem feitas adiciona esse token
+        /// dentro do Authorization
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var authorizationHeader = _user.ObterHttpContext().Request.Headers["Authorization"];
+
+            if (!string.IsNullOrEmpty(authorizationHeader))
+            {
+                request.Headers.Add("Authorization", new List<string>() {authorizationHeader});
+            }
+
+            var token = _user.ObterUserToken();
+
+            if (token != null)
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+            
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
