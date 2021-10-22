779b21fb2ef40dc3a5b03c2438501af2900e044c
fillipe.felix <felipesoares_1993@hotmail.com>
2021-10-21 21:36:49 -0300

    Defining the Client and Client API Address entities

---
+++ src/services/EE.Cliente.API/Models/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using EE.Core.DomainObjects;
+
+namespace EE.Cliente.API.Models
+{
+    /// <summary>
+    /// Classe Cliente
+    /// </summary>
+    public class Cliente : Entity, IAggregateRoot
+    {
+        /// <summary>
+        /// Construtor Cliente
+        /// </summary>
+        /// <param name="nome"></param>
+        /// <param name="email"></param>
+        /// <param name="cpf"></param>
+        /// <param name="excluido"></param>
+        public Cliente(string nome, string email, string cpf, bool excluido)
+        {
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+            Excluido = false;
+        }
+        
+        /// <summary>
+        /// EF Relacionamento
+        /// </summary>
+        protected Cliente(){ }
+
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+        public bool Excluido { get; private set; }
+        public Endereco Endereco { get; private set; }
+    }
+}
---
+++ src/services/EE.Cliente.API/Models/Endereco.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using EE.Core.DomainObjects;
+
+namespace EE.Cliente.API.Models
+{
+    /// <summary>
+    /// Classe Endereço
+    /// </summary>
+    public class Endereco : Entity
+    {
+        
+        /// <summary>
+        /// Construtor Endereco
+        /// </summary>
+        /// <param name="logradouro"></param>
+        /// <param name="numero"></param>
+        /// <param name="complemento"></param>
+        /// <param name="bairro"></param>
+        /// <param name="cep"></param>
+        /// <param name="cidade"></param>
+        /// <param name="estado"></param>
+        public Endereco(string logradouro, string numero, string complemento, string bairro, string cep, string cidade, string estado)
+        {
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cep = cep;
+            Cidade = cidade;
+            Estado = estado;
+        }
+        
+        public string Logradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Complemento { get; private set; }
+        public string Bairro { get; private set; }
+        public string Cep { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
+        public Guid ClienteId { get; private set; }
+        // EF Relacionamento
+        // Relacionamento de 1:1
+        public Cliente Cliente { get; set; }
+    }
+}
